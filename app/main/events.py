9ed58df9187b61f03ab64af414768565b598cca1
DavidSchott <daschott@iastate.edu>
2016-07-15 18:49:57 -0500

    Added simple Eliza chatbot.

--- app/main/events.py
+++ app/main/events.py
@@ -1,6 +1,7 @@
 from flask import session
 from flask.ext.socketio import emit, join_room, leave_room
 from .. import socketio
+from bot import Bot
 
 
 @socketio.on('joined', namespace='/chat')
@@ -10,6 +11,11 @@ def joined(message):
     room = session.get('room')
     join_room(room)
     emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+    if int(room) % 10 == 0:
+        b = Bot()
+        # Greet user
+        emit('status', {'msg': b.name() + ' [BOT] has entered the room.'}, room=room)
+        emit('message', {'msg':  b.name() + ': ' + b.greet()}, room=room)
 
 
 @socketio.on('text', namespace='/chat')
@@ -17,7 +23,11 @@ def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
     room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+    emit('message', {'msg': session.get('name') + ': ' + message['msg']}, room=room)
+    if int(room) % 10 == 0:
+        b = Bot()
+        # Talk to user
+        emit('message', {'msg': b.name() + ': ' + b.respond(message['msg'])}, room=room)
 
 
 @socketio.on('left', namespace='/chat')
--- bot.py
+++ bot.py
@@ -1,22 +1,58 @@
-from nltk.chat.eliza import eliza_chat
-from nltk.chat.iesha import iesha_chat
-from nltk.chat.rude import rude_chat
-from nltk.chat.suntsu import suntsu_chat
-from nltk.chat.zen import zen_chat
+import nltk.chat.eliza as el
+import nltk.chat.iesha as ie
+import nltk.chat.suntsu as sun
+import nltk.chat.zen as zen
+import nltk.chat.rude as rude
+from nltk.chat import util
 import sys
 
-nltk_bot_lookup = {"suntsu": suntsu_chat,"eliza":eliza_chat,"teen":iesha_chat,"rude":rude_chat,"zen":zen_chat}
 
-try:
-    bot_type = sys.argv.pop()
-except IndexError:
-    raise IndexError("No bot type specified.")
+class Bot:
+    def __init__(self, bot_name="eliza"):
+        if bot_name.lower() == "sun":
+            self._bot = util.Chat(sun.pairs, sun.reflections)
+            self._greetings = "You seek enlightenment?"
+            self._name = "Sun Tsu"
+        elif bot_name.lower() == "iesha":
+            self._bot = util.Chat(ie.pairs, ie.reflections)
+            self._greetings = "hi!! i'm iesha! who r u??!"
+            self._name = "*iesha*"
+        elif bot_name.lower() == "zen":
+            self._bot = util.Chat(zen.pairs, zen.reflections)
+            self._greetings = "Look beyond mere words and letters - look into your mind"
+            self._name = "Zen Master"
+        elif bot_name.lower() == "rude":
+            self._bot = util.Chat(rude.pairs, rude.reflections)
+            self._greetings = "Wow, I look cool today. Hey!"
+            self._name = "Chad"
+        else:
+            self._bot = util.Chat(el.pairs, el.reflections)
+            self._greetings = "Hello.  How are you feeling today?"
+            self._name = "Eliza"
 
-if not bot_type.lower() in nltk_bot_lookup.keys():
-    raise ValueError("Unknown bot-type {0}. Please select from the following:".format(bot_type))
+    def respond(self, query):
+        return self._bot.respond(query)
 
+    def greet(self):
+        return self._greetings
 
-print("Hi, you are chatting to NLTK's {0} Chatbot.\n"
-      "Simply press Ctrl-D to exit.\n".format(bot_type))
+    def name(self):
+        return self._name
 
-nltk_bot_lookup.get(bot_type)()
+""""
+def main():
+    nltk_bot_lookup = {"suntsu": sun.suntsu_chat, "eliza": el.eliza_chat, "teen": ie.iesha_chat, "rude": rude.rude_chat,
+                       "zen": zen.zen_chat}
+    try:
+        bot_type = sys.argv.pop()
+    except IndexError:
+        raise IndexError("No bot type specified.")
+    if not bot_type.lower() in nltk_bot_lookup.keys():
+        raise KeyError("Unknown bot-type {0}".format(bot_type))  # TODO: Please select from the following items?
+    print("Hi, you are chatting to NLTK's {0} Chatbot.\n"
+          "Simply press Ctrl-D to exit.\n".format(bot_type))
+    nltk_bot_lookup.get(bot_type)()
+
+if __name__ == '__main__':
+    main()
+"""
