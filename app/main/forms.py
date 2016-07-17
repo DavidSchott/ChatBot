581d842653cd3fe6f4916aa46a2a006af165784f
DavidSchott <daschott@iastate.edu>
2016-07-17 09:35:29 -0500

    Added background photo and centered panel.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,11 @@
 from flask.ext.wtf import Form
-from wtforms.fields import StringField, SubmitField, RadioField
+from wtforms.fields import StringField, SubmitField, RadioField, SelectField
 from wtforms.validators import Required
 
 class LoginForm(Form):
     """Accepts a nickname and a room."""
     name = StringField('Name', validators=[Required()])
-    room = RadioField(u'Select a ChatBot', choices=[
+    room = SelectField(u'Select a ChatBot', choices=[
         ('60', 'Chatty [Statistical, Continous]'),
         ('10', 'Therapist [Pattern-matching]'), ('20', 'Sun Tsu [Pattern-matching]'),
         ('50', 'Arrogant [Pattern-matching]'), ('30', 'Teen [Pattern-matching]'),
