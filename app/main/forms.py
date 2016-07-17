7e673d94cafe126e29aaeeac9282280010ed359f
DavidSchott <daschott@iastate.edu>
2016-07-17 10:24:24 -0500

    Added chat-background

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,7 +6,7 @@ class LoginForm(Form):
     """Accepts a nickname and a room."""
     name = StringField('Name', validators=[Required()])
     room = SelectField(u'Select a ChatBot', choices=[
-        ('60', 'Chatty [Statistical, Continous]'),
+        ('60', 'Chatty [Statistical, Evolving]'),
         ('10', 'Therapist [Pattern-matching]'), ('20', 'Sun Tsu [Pattern-matching]'),
         ('50', 'Arrogant [Pattern-matching]'), ('30', 'Teen [Pattern-matching]'),
         ('40', "Philosopher [Pattern-matching]"), ('1', 'None [Public]')
