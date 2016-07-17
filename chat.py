9433579db2a539d22c3d658cad05ada593c1b887
DavidSchott <daschott@iastate.edu>
2016-07-16 20:31:16 -0500

    Attempt on fixing procfile

--- chat.py
+++ chat.py
@@ -1,7 +1,10 @@
 #!/bin/env python
 from app import create_app, socketio
+import os
+
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, port=port)
