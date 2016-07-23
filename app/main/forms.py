ff61d01b812e48dd50c233faa24522ccdcb96643
DavidSchott <daschott@iastate.edu>
2016-07-22 20:13:12 -0500

    Replaced dropdown Bot selection with avatars.

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,10 +5,11 @@ from wtforms.validators import Required
 class LoginForm(Form):
     """Accepts a nickname and a room."""
     name = StringField('Name', validators=[Required()])
-    room = SelectField(u'Select a ChatBot', choices=[
+    room = StringField('Room', validators=[Required()])
+    """SelectField(u'Select a ChatBot', choices=[
         ('60', 'Chatty [Statistical, Evolving]'),
         ('10', 'Therapist [Pattern-matching]'), ('20', 'Sun Tsu [Pattern-matching]'),
         ('50', 'Arrogant [Pattern-matching]'), ('30', 'Teen [Pattern-matching]'),
         ('40', "Philosopher [Pattern-matching]"), ('1', 'None [Public]')
-    ])
+    ])"""
     submit = SubmitField('Enter Chatroom')
